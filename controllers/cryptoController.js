a34f8f397575fd6b789176fc791a8be3c1690262
SilverFlame83 <imren_83@abv.bg>
2023-04-08 11:56:27 +0300

    edit added

--- controllers/cryptoController.js
+++ controllers/cryptoController.js
@@ -44,7 +44,7 @@ router.post("/create", isUser(), async (req, res) => {
   }
 });
 
-router.get("/details/:id", async (req, res) => {
+router.get("/details/:id", isUser(), async (req, res) => {
   const crypto = await req.storage.getCryptoById(req.params.id);
 
   crypto.isOwner = req.user && req.user._id == crypto.owner._id;
@@ -52,6 +52,37 @@ router.get("/details/:id", async (req, res) => {
   crypto.bought = req.user && crypto.boughtBy.find((u) => u == req.user._id);
 
   res.render("details", { crypto });
+});
+
+router.get("/edit/:id", isUser(), async (req, res) => {
+  try {
+    const crypto = await req.storage.getCryptoById(req.params.id);
+
+    res.render("edit", { crypto });
+  } catch (err) {
+    console.log(err.message);
+    res.redirect("/crypto/details/" + req.params.id);
+  }
+});
+
+router.post("/edit/:id", isUser(), async (req, res) => {
+  try {
+    await req.storage.editCrypto(req.params.id, req.body);
+    res.redirect("/crypto/details/" + req.params.id);
+  } catch (err) {
+    const ctx = {
+      errors: parseError(err),
+      crypto: {
+        _id: req.params.id,
+        name: req.body.name,
+        imageUrl: req.body.imageUrl,
+        price: req.body.price,
+        description: req.body.description,
+        payment: req.body.payment,
+      },
+    };
+    res.render("edit", ctx);
+  }
 });
 
 router.get("/delete/:id", async (req, res) => {
@@ -64,17 +95,17 @@ router.get("/delete/:id", async (req, res) => {
   }
 });
 
-router.get('/buy/:id', isUser(), async (req,res)=>{
+router.get("/buy/:id", isUser(), async (req, res) => {
   try {
-      //const buy = await req.storage.getGameById(req.params.id);
-      
-      //console.log('User',req.user)
-      await req.storage.buyCrypto(req.params.id, req.user._id);
-    
-      res.redirect('/crypto/details/' + req.params.id);
+    //const buy = await req.storage.getGameById(req.params.id);
+
+    //console.log('User',req.user)
+    await req.storage.buyCrypto(req.params.id, req.user._id);
+
+    res.redirect("/crypto/details/" + req.params.id);
   } catch (err) {
-      console.log('Éroor',err.message);
-      res.redirect('/crypto/details/'+ req.params.id)
+    console.log("Éroor", err.message);
+    res.redirect("/crypto/details/" + req.params.id);
   }
 });
 
